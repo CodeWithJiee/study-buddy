94c420b79f78aa4ef2857fc796f1a7fdfa270499
codeWithJie <kenjie.deguzman.bsit@gmail.com>
2025-12-02 11:25:03 +0800

    Page changes

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Login() {
+    return (
+        
+        <section>
+            <div>
+                <Link  href="/">Home</Link>
+            </div>
+           <div> Login Page</div>
+        </section>
+        
+    )
+}
